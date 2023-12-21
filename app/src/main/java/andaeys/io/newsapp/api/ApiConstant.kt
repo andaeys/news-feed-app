091f09ee7c87036a6cbad660536f6d086690a88f
andaeys <ekayudha.sulistianda@quipper.com>
2023-12-21 13:42:26 +0700

    implement simple ui render

--- app/src/main/java/andaeys/io/newsapp/api/ApiConstant.kt
+++ app/src/main/java/andaeys/io/newsapp/api/ApiConstant.kt
@@ -1,6 +1,6 @@
 package andaeys.io.newsapp.api
 
 object ApiConstant {
-    const val COUNTRY = "us"
+    const val COUNTRY = "id"
     const val API_KEY = "c6bb54c768c445ac9a7e188ac9a67e5f"
 }
--- app/src/main/java/andaeys/io/newsapp/view/TopNewsActivity.kt
+++ app/src/main/java/andaeys/io/newsapp/view/TopNewsActivity.kt
@@ -4,14 +4,41 @@ import andaeys.io.newsapp.model.Article
 import andaeys.io.newsapp.model.state.TopNewsState
 import andaeys.io.newsapp.ui.theme.TopHeadlineNewsTheme
 import andaeys.io.newsapp.viewmodels.TopNewsViewModel
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class TopNewsActivity : ComponentActivity() {
@@ -33,12 +60,10 @@ class TopNewsActivity : ComponentActivity() {
 
         }
     }
-
-    override fun onResume() {
-        super.onResume()
-    }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopNewsScreen(
     state: TopNewsState,
@@ -49,6 +74,108 @@ fun TopNewsScreen(
     LaunchedEffect(Unit){
         onRefresh()
     }
-    Log.d("hello", state.toString())
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Top News") },
+                actions = {
+                    IconButton(onClick = { onRefresh() }) {
+                        Icon(Icons.Default.Refresh, contentDescription = "Refresh")
+                    }
+                }
+            )
+        },
+        content = {
+            when (state) {
+                is TopNewsState.Loading -> {
+                    // Loading state
+                    LoadingScreen()
+                }
+                is TopNewsState.Success -> {
+                    // Display list of articles
+                    ArticleList(state.articleList, onArticleClick)
+                }
+                is TopNewsState.Empty -> {
+                    // Empty state
+                    EmptyState()
+                }
+                is TopNewsState.Error -> {
+                    // Error state
+                    ErrorState(state.errorMessage) {
+                        // Retry button
+                        onRefresh()
+                    }
+                }
+            }
+        }
+    )
+
 }
 
+@Composable
+fun LoadingScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun ArticleList(articles: List<Article>, onArticleClick: (Article) -> Unit) {
+    LazyColumn {
+        items(articles) { article ->
+            ArticleListItem(article = article, onArticleClick = { onArticleClick(article) })
+            Divider(modifier = Modifier.padding(horizontal = 16.dp), color = Color.Gray)
+        }
+    }
+}
+
+@Composable
+fun ArticleListItem(article: Article, onArticleClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onArticleClick() }
+            .padding(16.dp)
+    ) {
+        Text(text = article.title, style = typography.titleMedium)
+    }
+}
+
+@Composable
+fun EmptyState() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("No top news available")
+    }
+}
+
+@Composable
+fun ErrorState(errorMessage: String, onRetry: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Error: $errorMessage")
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = { onRetry() }) {
+                Text("Retry")
+            }
+        }
+    }
+}
