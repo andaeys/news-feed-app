3ac6d10a6564f5e4a2f6ba14e3dea12d7813c765
andaeys <ekayudha.sulistianda@quipper.com>
2024-02-07 14:42:53 +0700

    make repo logic run on IO thread

--- app/src/main/java/andaeys/io/newsapp/repository/TopNewsRepositoryImpl.kt
+++ app/src/main/java/andaeys/io/newsapp/repository/TopNewsRepositoryImpl.kt
@@ -2,15 +2,17 @@ package andaeys.io.newsapp.repository
 
 import andaeys.io.newsapp.api.ApiService
 import andaeys.io.newsapp.model.TopNews
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class TopNewsRepositoryImpl(private val apiService: ApiService) : TopNewsRepository {
 
-    override suspend fun fetchTopNews(): TopNews {
+    override suspend fun fetchTopNews(): TopNews = withContext(Dispatchers.IO) {
         val response = apiService.getTopHeadlines()
         if (response.status!="ok") {
             throw Exception("Error fetching top news")
         }
-        return TopNews(
+        TopNews(
             articles = response.articles
         )
     }
